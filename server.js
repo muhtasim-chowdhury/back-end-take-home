daa55e9acc386743c84a90e370b5bc375e55b426
thegodofwar9732 <muhtasimmc@gmail.com>
2020-12-04 21:01:39 -0500

    finished routes

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const fetch = require('node-fetch')
+const PORT = 5000
+let nextAvailableId = 1
+const docObj = {}
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/request', (req, res) => {
+	const {body} = req.body
+	// initiate request to third party service
+	// create unique identifier for this request
+	const id = nextAvailableId++
+	docObj.id = {body, time, createdTimestamp: new Date().getTime()}
+	fetch('http://example.com/request', {
+		method: 'POST', 
+		body: JSON.stringify({body, callback: `/callback/${id}`})
+	})
+	res.send(body)
+})
+
+app.post('/callback/:id', (req, res) => {
+	const {id} = req.params 
+
+	docObj[id].status = req.body // STARTED
+	docObj[id].modifiedTimeStamp = new Date().getTime()
+	res.sendStatus(204)
+})
+
+app.put('/callback/:id', (req, res)=> {
+	const {id} = req.params 
+	const {status, detail} = req.body
+	
+	docObj[id].status = status
+	docObj[id].detail = detail
+	docObj[id].modifiedTimeStamp = new Date().getTime()
+
+	res.sendStatus(204)
+})
+
+app.get('/status/:id', (req, res)=> {
+	const {id} = req.params 
+	res.json(docObj[id])
+})
+
+app.listen(PORT, () => {
+	console.log(`Example app listening at http://localhost:${PORT}`)
+})
